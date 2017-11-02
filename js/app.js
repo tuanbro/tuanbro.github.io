286a47baf39eea919b09bf91bb7d1f4cc60cc999
[Hello World] <chaungo@users.noreply.github.com>
2017-11-02 16:30:25 +0700

    resize toolbar
    resize toolbar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,2 @@
 var app = angular.module('App', ['ngMaterial', 'ngResource', 'ngAnimate']);
-app.config(function ($mdThemingProvider) {
-    $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('red');
 
-});
-
-app.controller('AppCtrl', function ($scope, $mdSidenav) {
-    $scope.toggleSidenav = function () {
-        return $mdSidenav('left').toggle();
-    };
-});
