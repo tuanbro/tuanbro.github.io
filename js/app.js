6eec14c7a6bcab0e2276f0473eb50e9c13e27655
[Hello World] <chaungo@users.noreply.github.com>
2017-11-15 15:38:53 +0700

    add search
    add search

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 var app = angular.module('App', ['ngMaterial', 'ngResource', 'ngAnimate']);
 
-app.controller('AppCtrl', function ($scope, $http,$resource) {
+app.controller('AppCtrl', function ($scope, $http, $resource, $filter) {
+    $scope.searchRes = [];
     $scope.searchBarVis = false;
-    $resource('js/data.json').get().$promise.then(function(data) {
+    $resource('js/data.json').get().$promise.then(function (data) {
         $scope.data = data;
-        console.log($scope.data.scene);
+        $scope.search = function () {
+            $scope.searchRes = [];
+            angular.forEach($scope.data.scene, function (value) {
+                if ($filter('lowercase')(value.name).indexOf($filter('lowercase')($scope.searchtext)) >= 0) {
+                    $scope.searchRes.push({
+                        name: value.name,
+                        thumb: value.thumb
+                    });
+                }
+            });
+        };
+
+        $scope.$watch('searchtext', function () {
+            if ($scope.searchtext !== undefined && $scope.searchtext !== '') {
+                $scope.search();
+            }
+        })
     });
+
 });
