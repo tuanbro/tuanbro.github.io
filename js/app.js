97a5cfa88ad218ead23697993a6ebcdc21a8ac3f
[Hello World] <chaungo@users.noreply.github.com>
2017-11-15 17:15:04 +0700

    add btn animation
    add btn animation

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 var app = angular.module('App', ['ngMaterial', 'ngResource', 'ngAnimate']);
 
 app.controller('AppCtrl', function ($scope, $http, $resource, $filter) {
+    $scope.cardwhiteframe = "3";
+    $scope.btnWhiteframe = "-1";
     $scope.searchRes = [];
     $scope.searchBarVis = false;
     $resource('js/data.json').get().$promise.then(function (data) {
         $scope.data = data;
+        console.log($scope.data.scene);
         $scope.search = function () {
             $scope.searchRes = [];
             angular.forEach($scope.data.scene, function (value) {
                 if ($filter('lowercase')(value.name).indexOf($filter('lowercase')($scope.searchtext)) >= 0) {
                     $scope.searchRes.push({
+                        id: value.id,
                         name: value.name,
                         thumb: value.thumb
                     });
@@ -21,7 +25,12 @@ app.controller('AppCtrl', function ($scope, $http, $resource, $filter) {
             if ($scope.searchtext !== undefined && $scope.searchtext !== '') {
                 $scope.search();
             }
-        })
+        });
+
+
+        $scope.click = function (value) {
+            console.log(value);
+        }
     });
 
 });
