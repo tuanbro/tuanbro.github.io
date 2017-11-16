62445aafa73882f739a38a1b99aa981ce9f6e310
lchthanhth615 <lchthanhth615@gmail.com>
2017-11-17 00:26:09 +0700

    enable info spots

--- js/tour2.js
+++ js/tour2.js
@@ -77,23 +77,22 @@ AFRAME.registerComponent( 'cth-info', {
   init: function () {
     var data = this.data;
     var el = this.el;
-
     // Check if we have the asset.
-    if ( document.getElementById( 'p' + data.target ) ) {
+    if ( document.getElementById( 'info' + data.target ) ) {
       el.addEventListener( 'click', function () {
         cthOverlay( data.target, data.pos, data.rot, data.width, data.height );
       });
     }
     else {
       this.el.setAttribute( 'model-opacity', "0.2" );
-      console.warn( 'Didnt find asset', data.target );
+      console.warn( 'Didnt find asset','info' + data.target );
     }
   },
   remove: function () {
     var data = this.data;
     var el = this.el;
     // Check if we have the asset.
-    if ( document.getElementById( 'p' + data.target ) ) {
+    if ( document.getElementById( 'info' + data.target ) ) {
       el.removeEventListener( 'click', function () {
         cthOverlay( data.target, data.pos, data.rot, data.width, data.height );
       });
@@ -116,7 +115,7 @@ function cthRejigMarkers () {
 
   // move the home button!
   // if ( '520' == window.skyes._id ) {
-  //   document.getElementById( 'home-wrapper' ).setAttribute( 'position', '0 0 0' );
+       document.getElementById( 'home-wrapper' ).setAttribute( 'visible', 'false' );
   //   document.getElementById( 'home-wrapper' ).setAttribute( 'rotation', '0 180 180' );
   // }
   // else {
@@ -130,67 +129,17 @@ function cthRejigMarkers () {
         var _rot = false;
         var _cm = data.scene[ window.skyes._id ];
 
-        var _wm = document.createElement( 'a-entity' );
-        _wm.setAttribute( 'class', 'link active' );
-        let spots = _cm.hotSpot;
+        var _container = document.createElement( 'a-entity' );
+        _container.setAttribute( 'class', 'link active' );
         var assets = document.querySelector('a-assets');
         
-        for (let i = 0; i < spots.length; i++) {
-            var _m = document.createElement( 'a-entity' );
-            _m.setAttribute( 'id', 'm-' + window.skyes._id + '-' + spots[i].toScn );
-            _m.setAttribute( 'class', 'marker' );
-            _m.setAttribute( 'model-opacity', 0 );
+        let spots = _cm.hotSpot;
+        initHotSpots(_container, spots, assets);
 
-            // if ( _cm.type && 'info' == _cm.type ) {
-
-            //   _m.setAttribute( 'obj-model', 'obj: #asterisk-obj; mtl: #asterisk-mtl' );
-            //   _m.setAttribute( 'scale', { x: 0.09, y: 0.09, z: 0.09 } );
-            //   _m.setAttribute( 'cth-info', 'target: ' + _cm.to + '; pos: ' + _cm.overlay.pos + '; rot: ' + _cm.overlay.rot + '; width: ' + _cm.overlay.width + '; height: ' + _cm.overlay.height + ';' );
-
-            //   var _rot = document.createElement( 'a-animation' );
-            //   _rot.setAttribute( 'easing', 'linear' );
-            //   _rot.setAttribute( 'attribute', 'rotation' );
-            //   _rot.setAttribute( 'repeat', 'indefinite' );
-            //   _rot.setAttribute( 'fill', 'forwards' );
-            //   _rot.setAttribute( 'from', '90 0 0' );
-            //   _rot.setAttribute( 'to', '90 0 -360' );
-            //   _rot.setAttribute( 'dur', 5000 );
-            //   _m.appendChild( _rot );
-            // }
-
-              _m.setAttribute( 'obj-model', 'obj: #ar-obj; mtl: #ar-mtl' );
-              _m.setAttribute( 'scale', { x: 0.2, y: 0.2, z: 0.2 } );
-              addToAssets('img', 'p' + spots[i].toScn, data.scene[ spots[i].toScn ].srcImg, assets);
-              _m.setAttribute( 'cth-swap', 'target: ' + spots[i].toScn + ';' );
-            
-
-
-            _m.setAttribute( 'position', spots[i].pos );
-            _m.setAttribute( 'rotation', spots[i].rot );
-                    var _av = document.createElement( 'a-animation' );
-            _av.setAttribute( 'begin', 'vanish' );
-            _av.setAttribute( 'easing', 'ease-in' );
-            _av.setAttribute( 'attribute', 'model-opacity' );
-            _av.setAttribute( 'fill', 'forwards' );
-            _av.setAttribute( 'from', 1 );
-            _av.setAttribute( 'to', 0 );
-            _av.setAttribute( 'dur', 250 );
-
-            var _aa = document.createElement( 'a-animation' );
-            _aa.setAttribute( 'begin', 'appear' );
-            _aa.setAttribute( 'easing', 'ease-out' );
-            _aa.setAttribute( 'attribute', 'model-opacity' );
-            _aa.setAttribute( 'fill', 'forwards' );
-            _aa.setAttribute( 'from', 0 );
-            _aa.setAttribute( 'to', 1 );
-            _aa.setAttribute( 'dur', 350 );
-
-            _m.appendChild( _av );
-            _m.appendChild( _aa );
-            _wm.appendChild( _m );
-        }     
-
-        _w.appendChild( _wm );
+        let infos = _cm.infoPot;
+        initInfoSpots(_container, infos, assets);
+      
+        _w.appendChild( _container );
       
 
       setTimeout( function () {
@@ -204,6 +153,70 @@ function cthRejigMarkers () {
   }, 500 );
 }
 
+function initHotSpots(container, spots, assets) {
+  for (let i = 0; i < spots.length; i++) {
+        var _hotspot = document.createElement( 'a-entity' );
+        _hotspot.setAttribute( 'id', 'm-' + window.skyes._id + '-' + spots[i].toScn );
+        _hotspot.setAttribute( 'class', 'marker' );
+        _hotspot.setAttribute( 'model-opacity', 0 );       
+
+        _hotspot.setAttribute( 'obj-model', 'obj: #ar-obj; mtl: #ar-mtl' );
+        _hotspot.setAttribute( 'scale', { x: 0.2, y: 0.2, z: 0.2 } );
+        addToAssets('img', 'p' + spots[i].toScn, data.scene[ spots[i].toScn ].srcImg, assets);
+       _hotspot.setAttribute( 'cth-swap', 'target: ' + spots[i].toScn + ';' );
+        
+        _hotspot.setAttribute( 'position', spots[i].pos );
+        _hotspot.setAttribute( 'rotation', spots[i].rot );
+
+        var _av = document.createElement( 'a-animation' );
+        _av.setAttribute( 'begin', 'vanish' );
+        _av.setAttribute( 'easing', 'ease-in' );
+        _av.setAttribute( 'attribute', 'model-opacity' );
+        _av.setAttribute( 'fill', 'forwards' );
+        _av.setAttribute( 'from', 1 );
+        _av.setAttribute( 'to', 0 );
+        _av.setAttribute( 'dur', 250 );
+
+        var _aa = document.createElement( 'a-animation' );
+        _aa.setAttribute( 'begin', 'appear' );
+        _aa.setAttribute( 'easing', 'ease-out' );
+        _aa.setAttribute( 'attribute', 'model-opacity' );
+        _aa.setAttribute( 'fill', 'forwards' );
+        _aa.setAttribute( 'from', 0 );
+        _aa.setAttribute( 'to', 1 );
+        _aa.setAttribute( 'dur', 350 );
+
+        _hotspot.appendChild( _av );
+        _hotspot.appendChild( _aa );
+        container.appendChild( _hotspot );
+    }    
+}
+
+function initInfoSpots(container, infos, assets) {
+  for (let i = 0; i < infos.length; i++) {
+        var _infoSpot = document.createElement( 'a-entity' );
+        _infoSpot.setAttribute( 'obj-model', 'obj: #asterisk-obj; mtl: #asterisk-mtl' );
+        _infoSpot.setAttribute( 'scale', { x: 0.09, y: 0.09, z: 0.09 } );
+        addToAssets('img', 'info' + infos[i].id, infos[i].img, assets);
+
+        _infoSpot.setAttribute( 'cth-info', 'target: ' + infos[i].id + '; pos: ' + infos[i].overlay.pos + '; rot: ' + infos[i].overlay.rot + '; width: ' + infos[i].overlay.width + '; height: ' + infos[i].overlay.height + ';' );
+
+        _infoSpot.setAttribute( 'position', infos[i].pos );
+        _infoSpot.setAttribute( 'rotation', infos[i].rot );
+        
+        var _rot = document.createElement( 'a-animation' );
+        _rot.setAttribute( 'easing', 'linear' );
+        _rot.setAttribute( 'attribute', 'rotation' );
+        _rot.setAttribute( 'repeat', 'indefinite' );
+        _rot.setAttribute( 'fill', 'forwards' );
+        _rot.setAttribute( 'from', '90 0 0' );
+        _rot.setAttribute( 'to', '90 0 -360' );
+        _rot.setAttribute( 'dur', 5000 );
+        _infoSpot.appendChild( _rot );
+
+        container.appendChild( _infoSpot );
+    }    
+}
 
 function cthOverlay ( target, pos, rot, width, height ) {
   var el = document.getElementById( target );
@@ -219,32 +232,13 @@ function cthOverlay ( target, pos, rot, width, height ) {
     _m.setAttribute( 'class', 'overlay' );
     //_m.setAttribute( 'model-opacity', 0 );
 
-    _m.setAttribute( 'src', '#p' + target );
+    _m.setAttribute( 'src', '#info' + target );
     _m.setAttribute( 'width', width );
     _m.setAttribute( 'height', height );
     _m.setAttribute( 'position', pos );
     _m.setAttribute( 'rotation', rot );
     _m.setAttribute( 'material', 'shader: flat; transparent: true;' );
-    /*
-    var _aa = document.createElement( 'a-animation' );
-    _aa.setAttribute( 'begin', 'appear' );
-    _aa.setAttribute( 'easing', 'ease-out' );
-    _aa.setAttribute( 'attribute', 'model-opacity' );
-    _aa.setAttribute( 'fill', 'forwards' );
-    _aa.setAttribute( 'from', 0 );
-    _aa.setAttribute( 'to', 1 );
-    _aa.setAttribute( 'dur', 350 );
-    var _av = document.createElement( 'a-animation' );
-    _av.setAttribute( 'begin', 'vanish' );
-    _av.setAttribute( 'easing', 'ease-out' );
-    _av.setAttribute( 'attribute', 'model-opacity' );
-    _av.setAttribute( 'fill', 'forwards' );
-    _av.setAttribute( 'from', 1 );
-    _av.setAttribute( 'to', 0 );
-    _av.setAttribute( 'dur', 350 );
-    _m.appendChild( _aa );
-    _m.appendChild( _av );
-    */
+   
     document.getElementById( 'poi' ).appendChild( _m );
 
     setTimeout( function () {
